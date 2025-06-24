a44dcb90dc1d336ee424f6cfc7b22ffd895e7bba
Parker2127 <shrikarudayarcs21@gmail.com>
2025-06-24 15:47:48 -0400

    Adding content to the Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 export default function Hero() {
   return (
-    <div>
-      Hero
+    <div className='min-h-screen flex flex-col gap-10 items-center justify'> {/* This just makes all the elements move to the center */}
+      <p> IT's TIME TO GET </p>
+      <h1> FIRED <span> UP </span></h1>
+      <p> I am insanely getting <span>fired up</span>, no one can stop me, I believe in myself and I am ready to tackle anything that comes in my way. It is <span>do or die</span> and I am definitely doing it.</p>
+      {/* Adding span will let us style the test inside span separately*/}
     </div>
   )
 }
