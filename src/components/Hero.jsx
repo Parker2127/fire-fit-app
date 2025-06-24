6c3823ee2a6fff61aeb752bf1859ea96ba3ca36c
Parker2127 <shrikarudayarcs21@gmail.com>
2025-06-24 15:50:17 -0400

    corrected code to center

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Hero() {
   return (
-    <div className='min-h-screen flex flex-col gap-10 items-center justify'> {/* This just makes all the elements move to the center */}
+    <div className='min-h-screen flex flex-col gap-10 items-center justify-center'> {/* This just makes all the elements move to the center */}
       <p> IT's TIME TO GET </p>
       <h1> FIRED <span> UP </span></h1>
       <p> I am insanely getting <span>fired up</span>, no one can stop me, I believe in myself and I am ready to tackle anything that comes in my way. It is <span>do or die</span> and I am definitely doing it.</p>
