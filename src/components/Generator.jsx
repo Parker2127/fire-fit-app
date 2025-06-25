5632d4945aaad0162d0e696d28367f95de0a258e
Parker2127 <shrikarudayarcs21@gmail.com>
2025-06-25 10:20:35 -0400

    Adding a component inside the Generator component

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import SectionWrapper from './SectionWrapper'
 
+
+function Header(props){
+    const { index, title, description } = props
+    return (
+        <div>
+            <div>
+                <p>{index} </p>
+                <h4>{title} </h4>
+            </div>
+            <p>{description}</p>
+        </div>
+    )
+}
+
 export default function Generator() {
   return (
    <SectionWrapper header={"generate your workout"} title= {['Enter', 'The', 'Dragon']}> {/* adding header for the use case of banners */}
-    SectionWrapper
+     <Header index={'01'} title="Pick your fire" description="Select the dragon you want to slay"/>
    </SectionWrapper>  /* Anything wrapped between the SectionWrapper component { parent tag } are considered to be children */
   )
 }
