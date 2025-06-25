bd77a3383f1898a0615c93e127dc48d8f0c3b1e4
Parker2127 <shrikarudayarcs21@gmail.com>
2025-06-25 09:56:54 -0400

    Added js file containing all type of workouts

--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -7,8 +7,10 @@ export default function SectionWrapper(props) {
     return (
         <section className='min-h-screen flex flex-col gap-10'>
             <div className='bg-slate-950 py-10 flex flex-col gap-2 justify-center items-center'>
-                <p>{header}</p>
-                <h2>{title[0]} <span>{title[1]}</span> {title[2]}</h2>     {/* using span so that we can style each word in the title separately */}
+                <p className='uppercase font-medium'>{header}</p>
+                <h2 className='font-semibold text-3xl sm:text-4xl md:text-5xl lg:text:6xl'>
+                    {title[0]} {title[1]} <span className='uppercase text-orange-600'>{title[2]}</span>
+                </h2>     {/* using span so that we can style each word in the title separately */}
             </div>
             {children}
         </section>
